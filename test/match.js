0c5d4a9de3bc48b8cb073fa474a9aacd4164056a
Justin Jose <justinjjose7@gmail.com>
2018-04-27 00:46:02 -0400

    minor edit to chris' program

--- test/match.js
+++ test/match.js
@@ -1,4 +1,4 @@
-const { matchUsers, matchUser_toUsers } = require("match");
+const { matchUsers, matchUser_toUsers } = require("../local_modules/match");
 
 const users = [
         {
@@ -12,10 +12,10 @@ const users = [
             "pwd": "4271e4587f2603887210508a0ffe1645",
             "__v": 0,
             "latLng": {
-                "lat": 40.7297312,
-                "lng": -73.98936179999998
+                "lat": 41.7297312,
+                "lng": -74.98936179999998
             },
-            "maxdist": 90,
+            "maxdist": 10,
             "desc": "i speak computer",
             "avatar": "https://res.cloudinary.com/dbormtzbg/image/upload/v1524783721/h9sl5jbc5w5fv3lnplvh.jpg",
             "surveyComplete": true,
@@ -23,8 +23,8 @@ const users = [
                 "sports": true,
                 "politics": false,
                 "food": false,
-                "popmusic": true,
-                "rap": false,
+                "popmusic": false,
+                "rap": true,
                 "religion": false
             }
         }
