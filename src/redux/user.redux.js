327e9790e87dd3f0b42cca7ca79b7adf275c0dac
Justin Jose <justinjjose7@gmail.com>
2018-04-30 13:38:40 -0400

    recover files

--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -4,6 +4,7 @@ const AUTH_SUCCESS = 'AUTH_SUCCESS'
 const LOGOUT = 'LOGOUT'
 const ERROR_MSG = 'ERROR_MSG'
 const LOAD_DATA = 'LOAD_DATA'
+const DATA_RETURN = 'DATA_RETURN'
 
 const initState = {
     //isAuth:'',
@@ -24,6 +25,8 @@ export function user(state=initState,action) {
             return {...state,isAuth:false,msg:action.msg}
         case LOGOUT:
             return {...initState, redirectTo:'/login'}
+        case DATA_RETURN:
+            return {...state, payload: action.payload}
         default:
             return state
     }
@@ -45,6 +48,10 @@ export function loadData(userinfo) {
 
 export function logoutSubmit() {
     return {type:LOGOUT}
+}
+
+export function returnMatchData(payload) {
+    return {type:DATA_RETURN, payload}
 }
 
 export function update(data) {
@@ -91,6 +98,21 @@ export function register({email, user, name, pwd, type}) {
                 }else {
                     dispatch(errorMsg(res.data.msg))
                 }
+            })
+    }
+}
+
+export function getMatches({user}) {
+    if(!user) {
+        return errorMsg('Missing fields')
+    }
+    return dispatch=>{
+        axios.post('/user/matchUser',{ user: user })
+            .then(res => {
+              if(res.status == 200){
+                console.log(res.data);
+                dispatch(returnMatchData(res.data))
+              }
             })
     }
 }
